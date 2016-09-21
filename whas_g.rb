7053d5d894eda52e0d09862f9d812e0b57145464
coolguy999 <agonzalez@chicagotechacademy.org>
2016-09-21 20:05:23 +0000

    i think i finished whatg

---
+++ whas_g.rb
@@ -0,0 +1,29 @@
+ a = -((5 + 9)*4)
+ puts a
+ 
+ # a/f = -7
+ f = -7 * a
+ puts f
+ 
+ # 7 + 3c = -11 
+ #3c = -11 -7
+ c = (-11 - 7)/3 
+ puts c
+
+ #b/5 + 9 = 6
+ b = (6 - 9 )*5 
+ b = (35 - 45)
+ puts b
+ 
+ # solving for e: b/e= 3
+ # e = 3 * b
+ e = b / 3
+ puts e
+
+ #c / d = 3
+ #c = d * 3 
+d = c / 3 
+ puts d
+ 
+ g = d + e + f 
+ puts g
